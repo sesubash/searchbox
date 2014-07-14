7e34b2afc58d0f6cf1f504f7f22f114889c3d1f6
subash_inkoniq <subash@inkoniq.com>
2014-07-14 10:29:20 +0530

    clean up

--- searchbox.js
+++ searchbox.js
@@ -1,22 +1,20 @@
 /*
- * Subash selvaraj 2014
- * http://subashflash.blogspot.in/
- * https://github.com/sesubash/searchbox
+ * Author      : Subash Selvaraj
+ * website     : http://subashflash.blogspot.in/
+ * Plugin url  : https://github.com/sesubash/searchbox
+ * Created date: 14-07-2014
+ * Last updated: 14-07-2014
  */
 
 (function ($) {
-
-	$.fn.searchbox = function(options){
-
-		var searchbox = this;
-		var defaults = {
-			wrapper: '<div class="searchbox-wrapper"/>',
+    $.fn.searchbox = function(options){
+        var searchbox = this;
+        var defaults = {
+            wrapper: '<div class="searchbox-wrapper"/>',
             method:"POST"
-		};
+        };
+        searchbox.settings = $.extend({}, defaults, options);
 
-		searchbox.settings = $.extend({}, defaults, options);
-        var $window = $(window);
-        
         searchbox.destroy = function(){
             searchbox.each(function(){
                 $.removeData(this);
@@ -29,43 +27,51 @@
         };
 
         return searchbox.each(function(i){
-
-        	var obj = $(this);
+            var obj = $(this);
             obj.addClass("search-box");
-        	// wrap the item with outer div
-        	obj.wrap(searchbox.settings.wrapper);
+            // wrap the item with outer parent div
+            obj.wrap(searchbox.settings.wrapper);
+            // add the input field to the plugin
             obj.append("<div class='input-wrapper'><input class='search-input' type='text' name='"+searchbox.settings.name+"'></div>");
+            // wrap the input field with a <form/> 
             obj.wrap("<form method='"+searchbox.settings.method+"' action='"+searchbox.settings.url+"'>");
-        	var $input = obj.find("input");
+
+            var $input = obj.find("input");
             var $inputWrapper = obj.find(".input-wrapper");
-        	var $parent = obj.parent().parent();
-        	$parent.on("click", function(e){
-        		$(this).toggleClass("active")
-			    $input.focus();
-			    e.preventDefault();
-			    e.stopPropagation();
-        	});
-            
+            var $parent = obj.parent().parent();
+
+            //
+            $parent.on("click", function(e){
+                $(this).toggleClass("active");
+                $input.focus();
+                e.preventDefault();
+                e.stopPropagation();
+            });
+
+            // set the postion of wrapper based on the option
             if(searchbox.settings.position !== "undefined"){
                 switch(searchbox.settings.position){
-                    case "right":       
+                    case "right":
                                 $inputWrapper.css("left", obj.width());
-                                $inputWrapper.css("top", -obj.height()/2);
                                 break;
                     case "left":
                                 $inputWrapper.css("right", obj.width());
-                                $inputWrapper.css("top", -obj.height()/2);
                                 break;
                 }
-            }else{
-                
+            }else{// by default set the position of wrapper to the right side of search icon
+                $inputWrapper.css("left", obj.width());
             }
 
+            // adjust the vertical position of wrapper
+            $inputWrapper.css("top", -obj.height()/2);
+
+            // prevent the search wrapper from closing it when clickon the input field
 			$input.on("click", function(e){
 			    e.preventDefault();
 			    e.stopPropagation();
 			});
 
+            // close the input wrapper on clicking anywhere else other than input box
 			$(document).on("click",function(){
 			    $parent.hasClass("active") ?  $parent.removeClass("active") : "";
 			});
